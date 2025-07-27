3975fb6c4ba01c52bc7df3c16ff87ce69bb05fb0
Teeye0 <tissahazong09@gmail.com>
2025-07-27 10:54:42 +0530

    added

---
+++ New folder/app.js
@@ -0,0 +1,15 @@
+let fruits=['apple', 'banana', 'pineapple','mango'];
+for(i of fruits){
+    console.log(i);
+}
+
+for(j of "apnaCollege"){
+    console.log(j);
+}
+
+let heroes= [['superman','batman','wonder woman'],['spiderman','ironman','thor']];
+for(list of heroes){
+    for(name of list){
+        console.log(name);
+    }
+}
